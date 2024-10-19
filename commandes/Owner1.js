b275171cbd7a510ecdf295d330fe9ae88d68ac38
YESSER TECH <aidenyassin115@gmail.com>
2024-10-19 16:13:07 +0300

    Update Owner1.js

--- commandes/Owner1.js
+++ commandes/Owner1.js
@@ -47,7 +47,7 @@ const date = moment().format('DD/MM/YYYY');
 // Vérification pour .jpeg ou .png
 else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
     try {
-        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Best coder md*, déveloper joelTech" }, { quoted: ms });
+        zk.sendMessage(dest, { image: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *yesser-md*, déveloper yessertech" }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
