e4dd21d1672148096711fc68db1f743ea9c6c478
YESSER TECH <aidenyassin115@gmail.com>
2024-10-19 16:04:22 +0300

    Update GroupQuotes.js

--- commandes/GroupQuotes.js
+++ commandes/GroupQuotes.js
@@ -4,7 +4,7 @@ const { zokou } = require('../framework/zokou');
 zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions) => {
   const { ms, repondre, verifGroupe, arg } = commandeOptions;
   if (!verifGroupe) {
-    repondre('This Command works in groups only!!!');
+    repondre('This Command works in groups only😂');
     return;
   }
 
@@ -13,13 +13,13 @@ zokou({ nomCom: 'quote', categorie: 'Group' }, async (dest, zk, commandeOptions)
       fetch('https://animechan.xyz/api/random')
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`*BONIPHACE-MDQuotes*
+          repondre(`*YESSER-MDQuotes*
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *BONIPHACE-MD*`);
+Powered by *yessertech*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
@@ -31,13 +31,13 @@ Powered by *BONIPHACE-MD*`);
       fetch('https://animechan.xyz/api/random/character?name=' + query)
         .then((response) => response.json())
         .then(async (quote) => {
-          repondre(`BONIPHACE-MD v5
+          repondre(`YESSER-MD
 
 🎬 Anime: ${quote.anime}
 👤 Character: ${quote.character}
 💬 Quote: ${quote.quote}
 
-Powered by *BONIPHACE-MD*`);
+Powered by *yessertech*`);
         });
     } catch (e) {
       repondre('Erreur lors de la génération de la citation : ' + e.message);
