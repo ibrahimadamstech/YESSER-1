915518540ead026ce170576308d313a943236bf4
YESSER TECH <aidenyassin115@gmail.com>
2024-10-22 00:21:46 +0300

    Update mesfonctions.js

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Boniphace tech
+/*_________by YESSER tech
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'BONIPHACE-MD',
+        pack: 'YESSER-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
