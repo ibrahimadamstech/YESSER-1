369ef595653adff42b29d51ee5ff1cdbd311aeba
YESSER TECH <aidenyassin115@gmail.com>
2024-11-10 23:20:49 +0300

    Update aliv.js

--- commandes/aliv.js
+++ commandes/aliv.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "test1", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "intro", reaction: "💀", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '*🤠* 👋 \n\n ' + " 💀 HELLOW FRIEND DON'T RUN THIS IS YESSER MD THE SIMPLE BOT CREATED BY 🤠 YESSERTECH 🤠 👉 SUPPORT amY YOUTUBE CHANNEL sub+like+comment 👉https://www.youtube.com/@Yesserboy92 🌛 THANKS";
     let d = '                                                                           𝐏yessertech is typing.......🤠✨';
